33d2987ce122983a175ff705dc5833251dc6c405
Phil S <philipstubbs13@gmail.com>
2017-12-16 12:20:45 -0600

    created object for questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,11 +3,29 @@
 //Variables to hold number of correct answers and incorrect answers at end of game.
 var correctAnswers;
 var incorrectAnswers;
+var questionSet = {
+	questionArray: [{
+		question: "Who won the first super bowl?",
+		choiceOne: "Green Bay Packers",
+		choiceTwo: "Kansas City Chiefs",
+		choiceThree: "New England Patriots",
+		choiceFour: "Los Angeles Rams"
+		}, {
+		question: "Who won Super Bowl MVP in 2017?",
+		choiceOne: "Dion Lewis",
+		choiceTwo: "Julio Jones",
+		choiceThree: "Matt Ryan",
+		choiceFour: "Tom Brady"
+	}]
+};
+
 
 //Append start button
-$("#start-button-div").append("<button id='start-game-button'>" + "Start" + "</button>");
+$("#start-div").append("<button id='start-game-button'>" + "Start" + "</button>");
 //Add styling to start button.
 $("#start-game-button").addClass("btn btn-primary btn-lg btn-block");
+//Hide question div at start of game
+$("#question-div").hide();
 //When start button is clicked, start game.
 $("#start-game-button").on("click", function() {
 	start();
@@ -16,5 +34,12 @@ $("#start-game-button").on("click", function() {
 
 //Functions
 function start() {
-
+	//Hide the div with start button...
+	$("#start-div").hide();
+	//Show the question div at start of game..
+	$("#question-div").show().append("<h2>" + "Question: " + questionSet.questionArray[0].question + "</h2>");
+	$("#question-div").show().append("<h2>" + questionSet.questionArray[0].choiceOne + "</h2>");
+	$("#question-div").show().append("<h2>" + questionSet.questionArray[0].choiceTwo + "</h2>");
+	$("#question-div").show().append("<h2>" + questionSet.questionArray[0].choiceThree + "</h2>");
+	$("#question-div").show().append("<h2>" + questionSet.questionArray[0].choiceFour + "</h2>");
 }
